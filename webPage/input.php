c8598fd71bf5e2bb4afb1e060745c78b5ee1c6b6
Cho young-ho <cyh_happy@naver.com>
2017-07-26 22:59:01 +0900

    update wepage/input.php

--- webPage/input.php
+++ webPage/input.php
@@ -1,3 +1,13 @@
 <?php
-  
+  $conn = mysqli_connect("localhost","root","nav30227.","HomeIoT");
+
+  $description = $_POST["description"];
+  $field1 = $_POST["field1"];
+  $field2 = $_POST["field2"];
+  $field3 = $_POST["field3"];
+
+  $sql="insert into devices (description,field1,field2,field3,time) values('$description','$field1','$field2','$field3',now());";
+  mysqli_query($mysqli, $conn);
+
+  mysqli_close($conn);
 ?>
