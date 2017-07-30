e8136260cda9cee78084908648c3623024030c54
Cho young-ho <cyh_happy@naver.com>
2017-07-30 22:39:02 +0900

    complete webPage/input.php

--- webPage/input.php
+++ webPage/input.php
@@ -1,16 +1,49 @@
 <?php
-  $conn = mysqli_connect("localhost","root","nav30227.","HomeIoT");
-  $description = $_GET["description"];
-  $field1 = $_POST["field1"];
-  $field2 = $_POST["field2"];
-  $field3 = $_POST["field3"];
+# insert into $description (field1,field2,field3,time) values($field1,$field2,$field3,DATE_ADD(now(),interval 9 hour))
+  $conn = mysqli_connect("127.0.0.1", "root", "nav30227.", "HomeIoT");
 
-  //$sql="insert into devices (description,field1,field2,field3,time) values('$description','$field1','$field2','$field3',now());";
-  //mysqli_query($conn, $spl);
+  $description=$_GET["description"];
 
-  $sql="select * from devices";
-  $result=mysqli_query($conn,$spl);
-  echo $result;
+  $sql1="insert into $description (";
+  $sql2=",time) values(";
+  $state=0;
+
+  $field1=$_GET["field1"];
+  $field2=$_GET["field2"];
+  $field3=$_GET["field3"];
+
+  if($_GET["field1"]){
+    $sql1=$sql1."field1";
+    $sql2=$sql2."$field1";
+    $state=1;
+  }
+  if($_GET["field2"]){
+    if ($state) {
+      $sql1=$sql1.",field2";
+      $sql2=$sql2.",$field2";
+    }else {
+      $sql1=$sql1."field2";
+      $sql2=$sql2."$field2";
+      $state=1;
+    }
+  }
+  if($_GET["field3"]){
+    if ($state) {
+      $sql1=$sql1.",field3";
+      $sql2=$sql2.",$field3";
+    }else {
+      $sql1=$sql1."field3";
+      $sql2=$sql2."$field3";
+      $state=1;
+    }
+  }
+
+
+
+  $sql=$sql1.$sql2.",DATE_ADD(now(),interval 9 hour))";
+
+  $result = mysqli_query($conn, $sql);
+  echo $sql;
 
   mysqli_close($conn);
 ?>
